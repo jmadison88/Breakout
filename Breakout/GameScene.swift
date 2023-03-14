9ce1f63b401f2a044a9f3fa07d62e6f36d99aad6
jmadison <jmad99@sbcglobal.net>
2023-03-14 09:55:54 -0500

    Setup display labels

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -142,8 +142,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
             let location = touch.location(in: self)
+            if playingGame {
                 paddle.position.x = location.x
-            
+            }
         }
     }
     func didBegin(_ contact: SKPhysicsContact) {
